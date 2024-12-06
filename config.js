492115d2b8c8f3f17224659254db315d175c7cc8
anhdhn1703 <damhuynamanh@gmail.com>
2024-12-06 16:59:48 +0700

    V01.01

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Kim Ngân à!',
+    introDesc: `Chào mừng em đến với thế giới của anh=)))))`,
+    btnIntro: '=))))',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh =))))',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại chọn anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Vì em thích anh đó, hihi <33333333',
+    mess: 'Anh biết mà 🥰. Love youuu 😘😘',
+    messDesc: 'Buổi hẹn đầu tiên em sẽ tỏ tình anh nhé=))))',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/messages/t/100032261959783'
+}
